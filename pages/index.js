48a8b61441811f2c24dd230b6da49f9e75e1efbe
Marwane <marwane.charfa@gmail.com>
2024-09-23 10:20:25 +0200

    suppression dossier pages

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Bienvenue sur votre application Next.js !</h1>
-        <p>Ceci est la page d'accueil.</p>
-      </div>
-    );
-  }
-  
