df5fdc09638e1f78243bd1a1e358a94157aed0d5
Marwane <marwane.charfa@gmail.com>
2024-09-23 10:18:27 +0200

    ini

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Bienvenue sur votre application Next.js !</h1>
+        <p>Ceci est la page d'accueil.</p>
+      </div>
+    );
+  }
+  
